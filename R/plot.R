93b664b18acffc9ec3de08d43bc80cb0c3e7cc15
kaneplusplus <kaneplusplus@gmail.com>
2021-08-09 19:09:00 -0400

    Separated plotting with ggplot2 with plotly.

--- R/plot.R
+++ R/plot.R
@@ -2,21 +2,35 @@
 #'
 #' @param x the data.frame object returned from a query.
 #' @importFrom purrr map_chr
+#' @importFrom stringi stri_split
 #' @export
 ctgov_gantt_labeller <- function(x) {
 
   link <- paste0("https://clinicaltrials.gov/ct2/show/NCT", x$nct_id)
   hrefs <- sprintf("NCTID: <a href=\"%s\">%s</a>", link, x$nct_id)
-  diseases <- map_chr(x$disease, ~paste0(.x$disease, collapse = "\n\t\t"))
-  drugs <- map_chr(x$drug, ~paste0(.x$drug, collapse = "\n\t\t"))
+  conditions <- 
+    map_chr(
+      x$conditions, 
+      ~ .x %>% 
+        stri_split(fixed = "|") %>%
+        unlist() %>%
+        paste(collapse = "\n\t\t")
+    )
+
+  interventions <- 
+    map_chr(
+      x$interventions, 
+      ~ paste(.x$name, collapse = "\n\t\t")
+    )
+    
   paste0(
     "</br>", "NCT ID: ", x$nct_id, "</br>",
-    "Status: ", x$status, "</br>",
-    "Source : ", x$source, "</br>",
+#    "Status: ", x$status, "</br>",
+    "Sponsor: ", x$sponsor, "</br>",
     "Start Date: ", x$start_date, "</br>",
     "Completion Date: ", x$primary_completion_date, "</br>",
-    "Disease:\n\t\t", diseases, "</br>",
-    "Treatments:\n\t\t", drugs, "</br>",
+    "Conditions:\n\t\t", conditions, "</br>",
+    "Interventions:\n\t\t", interventions, "</br>",
     "Enrollment: ", x$enrollment, "</br>"
   )
 }
@@ -31,13 +45,9 @@ ctgov_gantt_labeller <- function(x) {
 #' @param label_column the column denoting the labels for the y-axis.
 #' (Default is "nct_id")
 #' @param color the column to be used for coloring. (Default is label_column)
-#' @param tooltip the tooltip to be shown. (Default is gantt_labeller(x))
-#' @param plotly should the visualization be rendered as a plotly object?
-#' (Default is FALSE)
 #'
 #' @importFrom ggplot2 ggplot aes geom_segment enexpr xlab ylab guides
 #' guide_legend
-#' @importFrom plotly ggplotly
 #' @export
 ctgov_plot_timeline <- function(
   x,
@@ -48,39 +58,43 @@ ctgov_plot_timeline <- function(
   tooltip = ctgov_gantt_labeller(x),
   plotly = FALSE) {
 
-  p <- ggplot(
-    data = x,
-    aes(
-      x = enexpr(start_date),
-      xend = eval(as.name(completion_date)),
-      y = eval(as.name(label_column)),
-      yend = eval(as.name(label_column)),
-      color = eval(as.name(color)),
-      text = tooltip
-    )
-  )
-  p <- p +
+  ggplot(
+      data = x,
+      aes(
+        x = enexpr(start_date),
+        xend = eval(as.name(completion_date)),
+        y = eval(as.name(label_column)),
+        yend = eval(as.name(label_column)),
+        color = eval(as.name(color)),
+        text = tooltip
+      )
+    ) +
     geom_segment(size = 3) +
     ylab(label_column) +
     xlab("Date") +
     guides(color = guide_legend(color))
+}
 
-  if (plotly) {
-    p <- ggplotly(p, tooltip = "text")
-    # TODO: As Ryan about the following
-    # p$x$data[[1]]$customdata <-
-    #   paste0("https://clinicaltrials.gov/ct2/show/", x[['nct_id']])
-    # p <- onRender(
-    #   p,
-    #   "
-    #     function(el,x){
-    #     el.on('plotly_click', function(d) {
-    #     var websitelink = d.points[0].customdata;
-    #     window.open(websitelink);
-    #     });
-    #     }
-    #     "
-    # )
-  }
-  p
+
+#' @title Convert a ctrialsgov Visualization to Plotly
+#'
+#' @param tooltip the tooltip to be shown. (Default is gantt_labeller(x))
+#' @importFrom plotly ggplotly
+#' @export
+ctgov_to_plotly <- function(p) {
+  ggplotly(p, tooltip = "text")
+  # TODO: As Ryan about the following
+  # p$x$data[[1]]$customdata <-
+  #   paste0("https://clinicaltrials.gov/ct2/show/", x[['nct_id']])
+  # p <- onRender(
+  #   p,
+  #   "
+  #     function(el,x){
+  #     el.on('plotly_click', function(d) {
+  #     var websitelink = d.points[0].customdata;
+  #     window.open(websitelink);
+  #     });
+  #     }
+  #     "
+  # )
 }
