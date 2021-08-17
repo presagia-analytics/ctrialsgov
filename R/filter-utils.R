644ca5494d9d2156de1f904f7ec5ddff7d56c8af
kaneplusplus <kaneplusplus@gmail.com>
2021-08-17 17:01:47 -0400

    New function for filtering trial sets.

---
+++ R/filter-utils.R
@@ -0,0 +1,10 @@
+
+#' Does a Term Appear in a Vector of Strings?
+#'
+#' @param s the vector of strings.
+#' @param pattern the pattern to search for.
+#' @param ignore_case should the case be ignored? Default TRUE
+#' @export
+has_term <- function(s, pattern, ignore_case = TRUE) {
+  any(grepl(pattern, s, ignore.case = ignore_case))
+}
